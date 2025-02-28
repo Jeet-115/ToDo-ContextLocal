335e78ad15ca7525d765ec1cb4b597d4011718db
Jeet115 <jeetmistry115@gmail.com>
2025-02-28 11:22:14 +0530

    update 4

--- ToDoContextLocal/src/components/index.js
+++ ToDoContextLocal/src/components/index.js
@@ -1,4 +1,4 @@
-import TodoForm from "./TodoForm";
-import TodoItem from "./TodoItem";
+export { default as ToDoForm } from './TodoForm';
+import TodoItem from "./ToDoItem";
 
 export {TodoForm, TodoItem}
